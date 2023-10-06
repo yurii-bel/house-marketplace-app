d511def401dbfb9eb0a50a5fd8685eb7f2315063
Yurii <yuriibel888@gmail.com>
2023-10-06 22:29:58 +0300

    feat: add google OAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -86,7 +87,8 @@ const SignIn = () => {
           </div>
         </form>
 
-        {/* Google 0Auth */}
+        <OAuth />
+
         <Link to="/sign-up" className="registerLink">
           Sign Up Instead
         </Link>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -11,6 +11,7 @@ import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -112,7 +113,8 @@ const SignUp = () => {
           </div>
         </form>
 
-        {/* Google 0Auth */}
+        <OAuth />
+
         <Link to="/sign-in" className="registerLink">
           Sign In Instead
         </Link>
