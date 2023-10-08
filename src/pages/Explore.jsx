95d875f1e467898546857f4ed7ec9ca59865c27d
Yurii <yuriibel888@gmail.com>
2023-10-08 21:12:09 +0300

    feat: add explore page & fetch listings from fb

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+
+      <main>
+        {/* Slider */}
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="rent"
+              className="exploreCategoryImg"
+            />
+          </Link>
+          <p className="exploreCategoryName">Places for rent</p>
+          <Link to="/category/sale">
+            <img
+              src={sellCategoryImage}
+              alt="sell"
+              className="exploreCategoryImg"
+            />
+          </Link>
+          <p className="exploreCategoryName">Places for sale</p>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Explore;
