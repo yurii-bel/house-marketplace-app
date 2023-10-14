f1e827e800477e5607e108a0c295397cec9840dc
Yurii <yuriibel888@gmail.com>
2023-10-14 15:37:58 +0300

    chore: remove unnecessary comment

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -41,7 +41,6 @@ const Category = () => {
         const listings = [];
 
         querySnap.forEach((doc) => {
-          console.log("Listing Data:", doc.data().name);
           return listings.push({
             id: doc.id,
             data: doc.data(),
